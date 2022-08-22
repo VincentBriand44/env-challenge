5a57a47f851730059a8fd13ab99278cc973782bb
Canon321 <vincent-briand_student2022@wilder.school>
2022-08-22 11:27:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-trvl85
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-trvl85)
---
+++ index.js
@@ -0,0 +1,7 @@
+// run `npm start` in the terminal
+
+require('dotenv').config();
+
+console.log(
+  `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`
+);
